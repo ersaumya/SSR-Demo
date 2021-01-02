d2005e74b80ca43cc9389baa57eff3f321e194c1
ersaumya <ersaumyarout@gmail.com>
2021-01-02 18:03:00 +0530

    final comment

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { pluck } from 'rxjs/operators';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
-
-  constructor() { }
+  id=this.route.params.pipe(pluck('id'));
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit(): void {
   }
